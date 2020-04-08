8f46e1b0834e1ca052e04763243b55c47a1ebc03
nas <nas.silver.bullet@gmail.com>
2020-04-09 00:56:26 +0900

    :+1: Update zigzag

--- zigzag_conversion.go
+++ zigzag_conversion.go
@@ -3,38 +3,22 @@ package main
 // 6. ZigZag Conversion
 // https://leetcode.com/problems/zigzag-conversion/
 func convert(s string, numRows int) string {
-	if numRows < 2 {
+	if len(s) == 0 || numRows == 0 {
+		return ""
+	}
+	if numRows == 1 {
 		return s
 	}
-	matrix := make([][]rune, numRows)
-	var row, col int
-	incre := numRows - 2
-	for _, c := range s {
-		switch col % (numRows - 1) {
-		case 0:
-			matrix[row] = append(matrix[row], c)
-			if row+1 == numRows {
-				col++
-				if incre == numRows-1 {
-					incre = 0
-				}
-				row = incre
-				continue
-			}
-			row++
-		default:
-			for i := 0; i < numRows; i++ {
-				if i != row {
-					continue
-				}
-				matrix[i] = append(matrix[i], c)
-			}
-			col++
-			row--
-			if row < 0 {
-				row = 0
-			}
+	matrix := make([][]byte, numRows)
+	var row int
+	step := 1
+	for i := 0; i < len(s); {
+		matrix[row] = append(matrix[row], s[i])
+		if (step == 1 && row == numRows-1) || (step == -1 && row == 0) {
+			step = -step
 		}
+		row += step
+		i++
 	}
 	var ret string
 	for _, arr := range matrix {
