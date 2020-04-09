9cf1aa0f70f2f3bb6365edaccb95a01560f86a9e
nas <nas.silver.bullet@gmail.com>
2020-04-09 23:59:31 +0900

    :+1: Update next permutation

--- next_permutation.go
+++ next_permutation.go
@@ -3,24 +3,21 @@ package main
 // 31. Next Permutation
 // https://leetcode.com/problems/next-permutation/
 func nextPermutation(nums []int) {
-	var (
-		i = len(nums) - 2
-		j int
-	)
-	for i >= 0 {
-		if nums[i] < nums[i+1] {
-			j = i + 1
-			for j < len(nums) && nums[j] > nums[i] {
-				j++
+	var k, l int
+	for k = len(nums) - 2; k >= 0; {
+		if nums[k] < nums[k+1] {
+			l = k + 1
+			for l < len(nums) && nums[l] > nums[k] {
+				l++
 			}
-			j--
-			nums[i], nums[j] = nums[j], nums[i]
+			l--
+			nums[k], nums[l] = nums[l], nums[k]
 			break
 		}
-		i--
+		k--
 	}
-	i++
-	for j = len(nums) - 1; i < j; i, j = i+1, j-1 {
-		nums[i], nums[j] = nums[j], nums[i]
+	k++
+	for l = len(nums) - 1; k < l; k, l = k+1, l-1 {
+		nums[k], nums[l] = nums[l], nums[k]
 	}
 }
